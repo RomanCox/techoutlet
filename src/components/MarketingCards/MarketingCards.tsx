c59c6ecfab1ee02a2871f71e76c92f40d68dcab9
Roman Cox <rom.cox.dev@gmail.com>
2025-09-07 15:11:03 +0300

    refactor MarketingCards block

--- src/components/MarketingCards/MarketingCards.tsx
+++ src/components/MarketingCards/MarketingCards.tsx
@@ -4,7 +4,7 @@ import { classNames } from '@/helpers/classNames'
 import { Picture } from '@/components/shared/Picture/Picture'
 import { Text } from '@/components/shared/Text/Text'
 
-import { CardPositionType, marketingCards } from '@/constants/constants'
+import { marketingCards } from '@/constants/constants'
 
 import cls from './MarketingCards.module.scss'
 
@@ -13,24 +13,6 @@ export const MarketingCards = memo(() => {
     <div className={cls.marketingCardsWrapper}>
       <div className={cls.marketingCardsContainer}>
         {marketingCards
-          .filter(({ cardPosition }) => cardPosition === CardPositionType.TOP)
-          .map(card => (
-            <div key={card.id} className={classNames(cls.card, {}, [cls[`card${card.id}`]])}>
-              <Text className={cls.cardText}>{card.text}</Text>
-              <Picture
-                png={card.imagePNG}
-                webp={card.imageWEBP}
-                alt={'not important image'}
-                className={cls.cardImage}
-                isCover
-              />
-            </div>
-          ))
-        }
-      </div>
-      <div className={cls.marketingCardsContainer}>
-        {marketingCards
-          .filter(({ cardPosition }) => cardPosition === CardPositionType.BOTTOM)
           .map(card => (
             <div key={card.id} className={classNames(cls.card, {}, [cls[`card${card.id}`]])}>
               <Text className={cls.cardText}>{card.text}</Text>
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,8 +4,8 @@ import ManyIphonesPNG from '@/assets/images/devices/many_iphones.png'
 import ManyIphonesWEBP from '@/assets/images/devices/many_iphones.webp'
 import PhoneBoxPNG from '@/assets/images/devices/phone_box.png'
 import PhoneBoxWEBP from '@/assets/images/devices/phone_box.webp'
-import IphonePNG from '@/assets/images/devices/iphone.png'
-import IphoneWEBP from '@/assets/images/devices/iphone.webp'
+import IphonesPNG from '@/assets/images/devices/2-iphones-reverse.png'
+import IphonesWEBP from '@/assets/images/devices/2-iphones-reverse.webp'
 import ActionCameraPNG from '@/assets/images/devices/action_camera.png'
 import ActionCameraWEBP from '@/assets/images/devices/action_camera.webp'
 import HeadphonesPNG from '@/assets/images/devices/headphones.png'
@@ -21,24 +21,11 @@ export const companyDescription: string[] = [
   'Open Box (новые товары в открытых или поврежденных коробках)',
 ]
 
-export enum ImagePositionType {
-  CENTER = 'center',
-  LEFT = 'left',
-  RIGHT = 'right'
-}
-
-export enum CardPositionType {
-  TOP = 'top',
-  BOTTOM = 'bottom',
-}
-
 interface IMarketingCards {
   id: number
   text: string
   imagePNG: StaticImageData
   imageWEBP: StaticImageData
-  imagePosition: ImagePositionType
-  cardPosition: CardPositionType
 }
 
 export const marketingCards: IMarketingCards[] = [
@@ -47,48 +34,36 @@ export const marketingCards: IMarketingCards[] = [
     text: 'Работаем с 2018 года',
     imagePNG: ManyIphonesPNG,
     imageWEBP: ManyIphonesWEBP,
-    imagePosition: ImagePositionType.CENTER,
-    cardPosition: CardPositionType.TOP,
   },
   {
     id: 2,
-    text: 'Прямые поставки с\nаукционов США/ЕС ОАЭ',
+    text: 'Доставка по всей\nБеларуси ',
     imagePNG: PhoneBoxPNG,
     imageWEBP: PhoneBoxWEBP,
-    imagePosition: ImagePositionType.RIGHT,
-    cardPosition: CardPositionType.TOP,
   },
   {
     id: 3,
-    text: 'Только оригинальная\nтехника',
-    imagePNG: IphonePNG,
-    imageWEBP: IphoneWEBP,
-    imagePosition: ImagePositionType.RIGHT,
-    cardPosition: CardPositionType.TOP,
+    text: 'Гарантия до 12 месяцев',
+    imagePNG: ActionCameraPNG,
+    imageWEBP: ActionCameraWEBP,
   },
   {
     id: 4,
-    text: 'Гарантия до 12 мес',
-    imagePNG: ActionCameraPNG,
-    imageWEBP: ActionCameraWEBP,
-    imagePosition: ImagePositionType.RIGHT,
-    cardPosition: CardPositionType.BOTTOM,
+    text: 'Только оригинальная\nтехника',
+    imagePNG: IphonesPNG,
+    imageWEBP: IphonesWEBP,
   },
   {
     id: 5,
     text: 'Самовывоз в\nцентре Минска',
     imagePNG: HeadphonesPNG,
     imageWEBP: HeadphonesWEBP,
-    imagePosition: ImagePositionType.RIGHT,
-    cardPosition: CardPositionType.BOTTOM,
   },
   {
     id: 6,
-    text: 'Доставка по всей\nБеларуси ',
+    text: 'Любые формы\nоплаты',
     imagePNG: NoteBookTransformerPNG,
     imageWEBP: NoteBookTransformerWEBP,
-    imagePosition: ImagePositionType.LEFT,
-    cardPosition: CardPositionType.BOTTOM,
   },
 ]
 
